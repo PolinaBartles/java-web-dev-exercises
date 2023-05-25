1b9cbc2a68b38bd153210f17a637ae9b7c47f942
Polina Bartles <polina.bartles@gmail.com>
2023-05-24 20:07:09 -0500

    Bonus mission

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -7,12 +7,28 @@ public class Area {
         double pi = 3.14;
         double r;
         Scanner input = new Scanner(System.in);
-    System.out.println("Enter a radius: ");
-    r = input.nextDouble();
+        while (true) {
+            System.out.println("Enter a radius: ");
+            String userInput = input.nextLine();
+
+            try {
+                r = Double.parseDouble(userInput);
+
+                if (r < 0 ) {
+                   System.out.println("Error: Radius cannot be negative. Please try again");
+                   continue;
+                }
+                A = Circle.getArea(r);
+                System.out.println("The area of a circle of radius  "+ r + "is: " + A);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Error: Invalid input. Please enter a valid number");
+            }
+        }
+
+
     input.close();
 
-    A = Circle.getArea(r);
-    System.out.println("The area of a circle of radius  "+ r + "is: " + A);
 
     }
 }
